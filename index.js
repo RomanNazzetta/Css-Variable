811f96bdef706a544a31276eae201fb5bf60bfa2
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-25 19:43:01 -0300

    changing whit JS

---
+++ index.js
@@ -0,0 +1,8 @@
+var root = document.querySelector(':root');
+var rootStyles = getComputedStyle(root);
+var red = rootStyles.getPropertyValue('--red');
+console.log('red: ', red);
+
+
+root.style.setProperty('--red', 'green');
+rootStyles.setProperty('--yellow', 'orange')
